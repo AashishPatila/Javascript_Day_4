9e881af39708a82b4b6153dca57c8cb03b793344
AashishPatila <aashish@deployer.com>
2022-12-16 17:56:33 +0530

    second

---
+++ Javascript_4.js
@@ -0,0 +1,121 @@
+
+// Question 1
+
+// function checkEligibile(age) 
+// {
+//     if (age >= 18) {
+//       document.write("Age is grater than 18.  ")
+//     }
+//     else {
+//       document.write("Age is smaller than 18.  ")
+//     }
+// }
+  
+//   let age = prompt("Enter your age: ");
+  
+//     if (checkEligibile(age))
+//     {
+//       document.write("You are eligible to vote!");
+//     }
+//    else
+//     {
+//       document.write("You are not eligible to vote");
+//     }
+
+// Question 2
+
+// function checkEligibility(age) {
+  
+//     if (age >= 18) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+  
+ 
+//   let age = prompt("Enter your age: ");
+//   let isEligible = checkEligibility(age);
+  
+ 
+//   let again = prompt("Do you want to enter another age to check? (yes/no)");
+  
+
+//   while (again === "yes") {
+//     age = prompt("Enter your age: ");
+//     isEligible = checkEligibility(age);
+//     again = prompt("again wants to Check? (yes/no)");
+//   }
+  
+  
+//   if (isEligible) {
+//     document.write("You are eligible to vote!");
+//   } else {
+//     document.write("You are not eligible to vote.");
+//   }
+
+// question 3
+
+
+// function checkEligibile(age) 
+// {
+//     if (age >= 18) {
+//       document.write("Age is grater than 18.  ")
+//     }
+//     else {
+//       document.write("Age is smaller than 18.  ")
+//     }
+// }
+  
+//   let age = prompt("Enter your age: ");
+  
+//     if (checkEligibile(age))
+//     {
+//       document.write("You are eligible to vote!");
+//     }
+//     else if (age >= 0 && age < 18)
+//    {
+//    alert("Alert age is less than 18")
+//    }
+//    else
+//     {
+//       document.write("You are eligible to vote");
+//     }
+
+
+//Question 4
+
+
+
+// let url = prompt("Enter website name want to visit.");
+// window.location.href=`https://www.${url}.com/`;
+
+
+
+
+ //Question  5
+ 
+
+// function Colour(){
+//     let bgColor = prompt("Please enter a background color");
+//     document.bgColor = bgColor;
+//     let textClr = prompt("Please enter text color.");
+//     let txt = document.lastChild.lastChild.firstElementChild;
+//     txt.style.color=textClr;
+// }
+
+
+// Question 6
+
+// function Colour()
+// {
+//   let fColor = prompt("Enter color name for first element");
+//   let el = document.lastElementChild.lastElementChild.firstElementChild;
+//   el.style.color=fColor;
+//   let lColor = prompt("Enter color name for last element");
+//   let lE = document.getElementsByClassName("frEl")[0];
+//   lE.style.color= lColor;
+// };
+
+// Quesation 7
+
